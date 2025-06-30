3f4901884da98565049bb5f9fb544e21a4e16979
Heet Mehta <heetmehta18125@gmail.com>
2025-06-30 14:57:11 +0530

    Initial commit

---
+++ Basics/Output.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Output {
+    
+      void Display()
+    {
+        System.out.println("Welcome to Java Programming!");
+
+    }
+}
---
+++ Basics/student.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class student {
+    public static void main(String[] args) {
+        Output o1 = new Output();
+        o1.Display();
+    }
+    
+}
